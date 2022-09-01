40a9bf2a2d9b6504c9dba54ee683c876584edfec
Sumit Ranjan <34721357+sumitsarraf@users.noreply.github.com>
2022-09-01 15:54:29 +0530

    7th commit

---
+++ angular-material/05/demos/angularmaterial/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'contactmanager', loadChildren: () => import('./contactmanager/contactmanager.module').then(m => m.ContactmanagerModule) },
+  { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule) },
+  { path: '**', redirectTo: 'contactmanager' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-material/07/demos/angularmaterial/src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -0,0 +1,46 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  public isScreenSmall: boolean;
+
+  users: Observable<User[]>;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private userService: UserService,
+    private router: Router) { }
+
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
+
+    this.users = this.userService.users;
+    this.userService.loadAll();
+    
+    this.router.events.subscribe(() => {
+      if (this.isScreenSmall) {
+        this.sidenav.close();
+      }
+    });
+  }
+
+}
